e29d8fd2116e63f5214fa38bc4a6f8b8860460af
Martin van Keulen <vankeulen.martin@gmail.com>
2024-12-08 21:10:01 +0100

    Day 7 part 2.

--- internal/day_7/day_7_test.go
+++ internal/day_7/day_7_test.go
@@ -19,7 +19,7 @@ func TestPartOne(t *testing.T) {
 func TestPartTwo(t *testing.T) {
 	result := SolvePartTwo(part1Input)
 
-	if result != "?" {
-		t.Errorf("day 7 part 2 should solve with '?' given input, not '%s'", result)
+	if result != "11387" {
+		t.Errorf("day 7 part 2 should solve with '11387' given input, not '%s'", result)
 	}
 }
--- internal/day_7/part_2.go
+++ internal/day_7/part_2.go
@@ -3,10 +3,47 @@ package day7
 import (
 	"bufio"
 	"fmt"
+	"strconv"
 	"strings"
 )
 
 func SolvePartTwo(input string) string {
-	_ = bufio.NewScanner(strings.NewReader(input))
-	return fmt.Sprint(0)
+	scanner := bufio.NewScanner(strings.NewReader(input))
+	result := int64(0)
+	for scanner.Scan() {
+		equation := scanner.Text()
+		equationParts := strings.Split(equation, ":")
+		expectedAnswer, _ := strconv.ParseInt(equationParts[0], 10, 64)
+
+		equationComponents := strings.Split(equationParts[1][1:], " ")
+
+		perms := generateOptions(len(equationComponents)-1, "*+|")
+
+		for _, options := range perms {
+			initialValue, _ := strconv.ParseInt(equationComponents[0], 10, 64)
+			partialResult := int64(initialValue)
+
+			for i, permutation := range options {
+				a, _ := strconv.ParseInt(equationComponents[i+1], 10, 64)
+
+				if permutation == '*' {
+					partialResult *= int64(a)
+				}
+				if permutation == '+' {
+					partialResult += int64(a)
+				}
+
+				if permutation == '|' {
+					newNumber, _ := strconv.ParseInt(fmt.Sprint(partialResult)+equationComponents[i+1], 10, 64)
+					partialResult = newNumber
+				}
+			}
+			if partialResult == expectedAnswer {
+				result += int64(expectedAnswer)
+				break
+			}
+		}
+	}
+
+	return fmt.Sprint(result)
 }
