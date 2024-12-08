41a64e4c7656bc6238f5c7da0aacc63b07450fdc
Martin van Keulen <vankeulen.martin@gmail.com>
2024-12-08 22:31:05 +0100

    Day 8 part 2

--- internal/day_8/day_8_test.go
+++ internal/day_8/day_8_test.go
@@ -19,7 +19,18 @@ func TestPartOne(t *testing.T) {
 func TestPartTwo(t *testing.T) {
 	result := SolvePartTwo(part1Input)
 
-	if result != "?" {
-		t.Errorf("day 8 part 2 should solve with '?' given input, not '%s'", result)
+	if result != "34" {
+		t.Errorf("day 8 part 2 should solve with '34' given input, not '%s'", result)
+	}
+}
+
+//go:embed test/simpler_example.txt
+var simplerExample string
+
+func TestSimplerExample(t *testing.T) {
+	result := SolvePartTwo(simplerExample)
+
+	if result != "6" {
+		t.Errorf("should solve with '6' given input, not '%s'", result)
 	}
 }
--- internal/day_8/part_2.go
+++ internal/day_8/part_2.go
@@ -7,6 +7,64 @@ import (
 )
 
 func SolvePartTwo(input string) string {
-	_ = bufio.NewScanner(strings.NewReader(input))
-	return fmt.Sprint(0)
+	scanner := bufio.NewScanner(strings.NewReader(input))
+
+	antennaLocations := make(map[rune][]Vector2)
+	antinodeLocations := make(map[Vector2]*interface{})
+	maxX := 0
+
+	y := 0
+	for scanner.Scan() {
+		line := scanner.Text()
+		maxX = len(line) - 1
+
+		for x, value := range line {
+			if value != '.' {
+				_, found := antennaLocations[value]
+
+				if !found {
+					antennaLocations[value] = make([]Vector2, 0)
+				}
+
+				antennaLocations[value] = append(antennaLocations[value], Vector2{x: x, y: y})
+			}
+		}
+
+		y++
+	}
+	maxY := y - 1
+
+	for _, locations := range antennaLocations {
+
+		if len(locations) > 1 {
+			for _, location := range locations {
+				antinodeLocations[location] = nil
+			}
+		}
+
+		for i := 0; i < len(locations); i++ {
+			for j := 0; j < len(locations); j++ {
+				if i == j {
+					continue
+				}
+				locationOne := locations[i]
+				locationTwo := locations[j]
+
+				for k := 1; true; k++ {
+					xD := (locationOne.x - locationTwo.x)
+					yD := (locationOne.y - locationTwo.y)
+
+					antinodeX := locationTwo.x + (xD * k)
+					antinodeY := locationTwo.y + (yD * k)
+
+					if antinodeX <= maxX && antinodeY <= maxY && antinodeX >= 0 && antinodeY >= 0 {
+						antinodeLocations[Vector2{antinodeX, antinodeY}] = nil
+					} else {
+						break
+					}
+				}
+			}
+		}
+	}
+	return fmt.Sprint(len(antinodeLocations))
 }
