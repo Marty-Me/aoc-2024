0ffc06f1e10d74fa51df5ec935c839c7d92dde73
Martin van Keulen <vankeulen.martin@gmail.com>
2024-12-09 09:55:24 +0100

    Day 9: set up skeleton.

---
+++ internal/day_9/part_1.go
@@ -0,0 +1,17 @@
+package day7
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+)
+
+type Vector2 struct {
+	x int
+	y int
+}
+
+func SolvePartOne(input string) string {
+	_ = bufio.NewScanner(strings.NewReader(input))
+	return fmt.Sprint(0)
+}
