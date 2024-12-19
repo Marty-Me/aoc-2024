ae025deaeab3328daf5f455595f0edc008752fa2
Martin van Keulen <vankeulen.martin@gmail.com>
2024-12-19 19:40:46 +0100

    Day 13 part 1.

--- internal/day_13/part_1.go
+++ internal/day_13/part_1.go
@@ -3,10 +3,97 @@ package day13
 import (
 	"bufio"
 	"fmt"
+	"strconv"
 	"strings"
 )
 
+type Vector2 struct {
+	x int
+	y int
+}
+
+type Game struct {
+	prize Vector2
+	a     Vector2
+	b     Vector2
+}
+
 func SolvePartOne(input string) string {
-	_ = bufio.NewScanner(strings.NewReader(input))
-	return fmt.Sprint(0)
+	scanner := bufio.NewScanner(strings.NewReader(input))
+	games := parseGames(scanner)
+	aCost := 3
+	bCost := 1
+
+	tokens := 0
+
+	for _, game := range games {
+		prizeX := game.prize.x
+		prizeY := game.prize.y
+
+		aX := game.a.x
+		bX := game.b.x
+
+		maxA := prizeX / aX
+		cost := -1
+		for a := maxA; a > 0; a-- {
+			if ((prizeX - (a * aX)) % bX) == 0 {
+				b := (prizeX - (a * aX)) / bX
+				if prizeY != (a*game.a.y)+(b*game.b.y) {
+					continue
+				}
+				if cost == -1 || (a*aCost)+(b*bCost) < cost {
+					cost = (a * aCost) + (b * bCost)
+				}
+
+			}
+		}
+
+		if cost > 0 {
+			tokens += cost
+		}
+	}
+
+	return fmt.Sprint(tokens)
+}
+
+func parseGames(scanner *bufio.Scanner) []Game {
+	games := make([]Game, 0)
+
+	var a Vector2
+	var b Vector2
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		if len(line) > 0 {
+			lineParts := strings.Split(line, ":")
+			identifier := lineParts[0]
+
+			coords := strings.Split(lineParts[1], ",")
+
+			if identifier == "Button A" {
+				x, _ := strconv.Atoi(strings.Split(coords[0], "+")[1])
+				y, _ := strconv.Atoi(strings.Split(coords[1], "+")[1])
+				a = Vector2{x, y}
+			}
+
+			if identifier == "Button B" {
+				x, _ := strconv.Atoi(strings.Split(coords[0], "+")[1])
+				y, _ := strconv.Atoi(strings.Split(coords[1], "+")[1])
+				b = Vector2{x, y}
+			}
+
+			if identifier == "Prize" {
+				x, _ := strconv.Atoi(strings.Split(coords[0], "=")[1])
+				y, _ := strconv.Atoi(strings.Split(coords[1], "=")[1])
+
+				games = append(games, Game{
+					prize: Vector2{x, y},
+					a:     a,
+					b:     b,
+				})
+			}
+		}
+	}
+	return games
 }
