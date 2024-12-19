23fffac0e9e321550f233f5aec62851e68aa557e
Martin van Keulen <vankeulen.martin@gmail.com>
2024-12-19 19:40:46 +0100

    Day 13 part 1.

--- internal/day_13/part_1.go
+++ internal/day_13/part_1.go
@@ -7,6 +7,26 @@ import (
 )
 
 func SolvePartOne(input string) string {
-	_ = bufio.NewScanner(strings.NewReader(input))
+	scanner := bufio.NewScanner(strings.NewReader(input))
+
+	prizes :=
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		if len(line) > 0 {
+			lineParts := strings.Split(line, " ")
+			identifier := lineParts[0]
+
+			if identifier == "Button" {
+				fmt.Println("Button")
+			}
+
+			if identifier == "Prize:" {
+				fmt.Println("Prize")
+			}
+
+		}
+	}
 	return fmt.Sprint(0)
 }
